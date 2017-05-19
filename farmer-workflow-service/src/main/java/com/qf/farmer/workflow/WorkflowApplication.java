35a88eea660935f64f346138edd9aad274ade4bf
qinfei <qf8505@163.com>
2017-05-19 15:02:41 +0800

    修改配置文件

--- farmer-workflow-service/src/main/java/com/qf/farmer/workflow/WorkflowApplication.java
+++ farmer-workflow-service/src/main/java/com/qf/farmer/workflow/WorkflowApplication.java
@@ -5,12 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * activiti工作流
+ * 
  * @author qinfei
  *
  */
 @SpringBootApplication
 public class WorkflowApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(WorkflowApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowApplication.class, args);
+	}
 }

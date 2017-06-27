d096b9a0adc2d31ad9ec1ec0011be89882d93e5d
qinfei <qf8505@163.com>
2017-06-27 19:47:52 +0800

    修改锁

--- farmer-store-service/src/main/java/com/qf/farmer/store/service/impl/StoreServiceImpl.java
+++ farmer-store-service/src/main/java/com/qf/farmer/store/service/impl/StoreServiceImpl.java
@@ -2,8 +2,11 @@ package com.qf.farmer.store.service.impl;
 
 import java.util.List;
 
+import javax.persistence.LockModeType;
+
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,7 +26,16 @@ public class StoreServiceImpl implements StoreService {
 	@Autowired
     private KafkaTemplate<String,String> kafkaTemplate;
 	
+	/**
+	 * 对店铺表使用读锁，@Lock(LockModeType.PESSIMISTIC_WRITE)  方式是写锁，写锁会导致其他事务无法进行并行锁操作
+	 * select ... FOR UPDATE同时只能有一个在语句执行，另一个会阻塞；select ... LOCK IN SHARE MODE可以多个同时执行
+	 * 特例：for udpate对使用唯一索引和主键这类保证唯一数据加锁(此类为行级锁，其他为表级锁)，不会对其他事物加锁有影响
+	 * @param storeName
+	 * @Query("select u from Store u where u.storeName= ?")  
+	 * @return
+	 */
 	@Override
+	@Lock(LockModeType.PESSIMISTIC_READ)
 	public Store saveStore(Store store) {
 		List<Store> list=null;
 		if(StringUtils.isNotBlank(store.getStoreName())){
